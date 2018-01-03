fb7fd7c00240e167d559ac260a256e0bc50d30ef
OwOb <35055831+OwOb@users.noreply.github.com>
2018-01-03 13:43:16 +0800

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+var bot = new Discord.Client();
+
+bot.on("ready", function() {
+	console.log("Ready");
+});
+
+bot.on("message", function(message) {
+	if (message.author.username == "OwO bot") return;
+	var s = message.content.split(" ");
+	var head = s[0].toLowerCase();
+	if(head == "!test") {
+		message.channel.sendMessage("本機正常運作中... ...");
+	}
+	else if (message.content.indexOf("誰是世界上最醜的人") != -1) {
+		message.channel.sendMessage(message.author.username+"是世界上最醜的人~~~  OwO");
+	}
+	//message.channel.sendMessage(message.author.username);
+});
+
+bot.login(process.env.BOT_TOKEN);
