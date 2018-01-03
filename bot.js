df4e72e85ad1ce7b6ba4d420dea6077ce42737a8
OwOb <35055831+OwOb@users.noreply.github.com>
2018-01-03 15:50:15 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,45 +18,33 @@ bot.on("message", function(message) {
 	var end = agar[agar.length-1].toLowerCase();
 	
 	if(head == "!test") {
-		message.channel.sendMessage("本機正常運作中... ...");
+		message.channel.sendMessage("本機正常運作中... ...");/*
 		command_cd[0] = 1;
-		setTimeout("command_cd[0] = 0", 5000);
+		setTimeout("command_cd[0] = 0", 5000);*/
 	}
 	
 	else if (head == "owo" || end == "owo") {
 		message.channel.sendMessage("-OwO- 喵？");
-		command_cd[1] = 1;
-		setTimeout("command_cd[1] = 0", 5000);
 	}
 	
 	else if (head == "-owo-" || end == "-owo-") {
 		message.channel.sendMessage("=OwO= 喵喵？");
-		command_cd[2] = 1;
-		setTimeout("command_cd[2] = 0", 5000);
 	}
 	
 	else if (head == "=owo=" || end == "=owo=") {
 		message.channel.sendMessage("≡OwO≡ 喵喵喵？");
-		command_cd[3] = 1;
-		setTimeout("command_cd[3] = 0", 5000);
 	}
 	
 	else if (head == "≡owo≡" || end == "≡owo≡") {
 		message.channel.sendMessage("≣OwO≣ 喵喵喵喵？");
-		command_cd[4] = 1;
-		setTimeout("command_cd[4] = 0", 5000);
 	}
 	
 	else if (head == "≣owo≣" || end == "≣owo≣") {
 		message.channel.sendMessage("OwO ？");
-		command_cd[5] = 1;
-		setTimeout("command_cd[5] = 0", 5000);
 	}
 	
 	else if (message.content.indexOf("誰是世界上最醜的人") != -1) {
 		message.channel.sendMessage(message.author.username+"是世界上最醜的人~~~  OwO");
-		command_cd[6] = 1;
-		setTimeout("command_cd[6] = 0", 5000);
 	}
 	//message.channel.sendMessage(message.author.username);
 });
