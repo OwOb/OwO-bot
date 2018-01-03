b1a40fe7d111cf503563795b9b93c5bdcf036254
OwOb <35055831+OwOb@users.noreply.github.com>
2018-01-03 16:09:58 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 var bot = new Discord.Client();
 
-var command_cd = new Array(1000);
+var command_cd = new Array();
 
 bot.on("ready", function() {
 	console.log("Ready");
@@ -17,34 +17,46 @@ bot.on("message", function(message) {
 	var head = agar[0].toLowerCase();
 	var end = agar[agar.length-1].toLowerCase();
 	
-	if(head == "!test" && !command_cd[0]) {
+	if(!command_cd[0] && head == "!test") {
 		message.channel.sendMessage("本機正常運作中... ...");
 		command_cd[0] = 1;
 		setTimeout(function(){command_cd[0] = 0;}, 5000);
 	}
 	
-	else if (head == "owo" || end == "owo") {
+	else if (!command_cd[1] && (head == "owo" || end == "owo")) {
 		message.channel.sendMessage("-OwO- 喵？");
+		command_cd[1] = 1;
+		setTimeout(function(){command_cd[1] = 0;}, 5000);
 	}
 	
-	else if (head == "-owo-" || end == "-owo-") {
+	else if (!command_cd[2] && (head == "-owo-" || end == "-owo-")) {
 		message.channel.sendMessage("=OwO= 喵喵？");
+		command_cd[2] = 1;
+		setTimeout(function(){command_cd[2] = 0;}, 5000);
 	}
 	
-	else if (head == "=owo=" || end == "=owo=") {
+	else if (!command_cd[3] && (head == "=owo=" || end == "=owo=")) {
 		message.channel.sendMessage("≡OwO≡ 喵喵喵？");
+		command_cd[3] = 1;
+		setTimeout(function(){command_cd[3] = 0;}, 5000);
 	}
 	
-	else if (head == "≡owo≡" || end == "≡owo≡") {
+	else if (!command_cd[4] && (head == "≡owo≡" || end == "≡owo≡")) {
 		message.channel.sendMessage("≣OwO≣ 喵喵喵喵？");
+		command_cd[4] = 1;
+		setTimeout(function(){command_cd[4] = 0;}, 5000);
 	}
 	
-	else if (head == "≣owo≣" || end == "≣owo≣") {
+	else if (!command_cd[5] && (head == "≣owo≣" || end == "≣owo≣")) {
 		message.channel.sendMessage("OwO ？");
+		command_cd[5] = 1;
+		setTimeout(function(){command_cd[5] = 0;}, 5000);
 	}
 	
-	else if (message.content.indexOf("誰是世界上最醜的人") != -1) {
+	else if (!command_cd[6] && message.content.indexOf("誰是世界上最醜的人") != -1) {
 		message.channel.sendMessage(message.author.username+"是世界上最醜的人~~~  OwO");
+		command_cd[6] = 1;
+		setTimeout(function(){command_cd[6] = 0;}, 5000);
 	}
 	//message.channel.sendMessage(message.author.username);
 });
