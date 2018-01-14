9707077dc581c9aba9209eb78fb2aa77faf27dee
OwOb <35055831+OwOb@users.noreply.github.com>
2018-01-14 15:54:17 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ bot.on("message", function(message) {
   }
   
   else if(!command_cd["!me"] && headlower == "!me") {
-    message.channel.sendMessage(message.member.roles.findAll("id",message.author.id)[0].name);
+    message.channel.sendMessage(message.member.roles.findAll("id",message.author.id) == null);
     command_cd["!me"] = 1;
     setTimeout(function(){command_cd["!me"] = 0;}, 5000);
   }
