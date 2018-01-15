5c604f242d52f2bbfb3e144e8cacbfdbb993caab
OwOb <35055831+OwOb@users.noreply.github.com>
2018-01-16 01:05:43 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,37 @@ var bot = new Discord.Client();
 
 var command_cd = new Array();
 
+function limitEval(code, fnOnStop, opt_timeoutInMS) {
+  var id = Math.random() + 1,
+    blob = new Blob(
+      ['onmessage=function(a){a=a.data;postMessage({i:a.i+1});postMessage({r:eval.call(this,a.c),i:a.i})};'],
+      { type:'text/javascript' }
+    ),
+    myWorker = new Worker(URL.createObjectURL(blob));
+  function onDone() {
+    URL.revokeObjectURL(blob);
+    fnOnStop.apply(this, arguments);
+  }
+  myWorker.onmessage = function (data) {
+    data = data.data;
+    if (data) {
+      if (data.i === id) {
+        id = 0;
+        onDone(true, data.r);
+      }
+      else if (data.i === id + 1) {
+        setTimeout(function() {
+          if (id) {
+            myWorker.terminate();
+            onDone(false);
+          }
+        }, opt_timeoutInMS || 1000);
+      }
+    }
+  };
+  myWorker.postMessage({ c: code, i: id });
+}
+
 bot.on("ready", function() {
   console.log("Ready");
 });
@@ -84,39 +115,6 @@ bot.on("message", function(message) {
   }
   
   else if(!command_cd["javascript"] && message.content.indexOf("javascript") == 0) {
-    function limitEval(code, fnOnStop, opt_timeoutInMS) {
-      var id = Math.random() + 1,
-        blob = new Blob(
-          ['onmessage=function(a){a=a.data;postMessage({i:a.i+1});postMessage({r:eval.call(this,a.c),i:a.i})};'],
-          { type:'text/javascript' }
-        ),
-        myWorker = new Worker(URL.createObjectURL(blob));
-
-      function onDone() {
-        URL.revokeObjectURL(blob);
-        fnOnStop.apply(this, arguments);
-      }
-
-      myWorker.onmessage = function (data) {
-        data = data.data;
-        if (data) {
-          if (data.i === id) {
-            id = 0;
-            onDone(true, data.r);
-          }
-          else if (data.i === id + 1) {
-            setTimeout(function() {
-              if (id) {
-                myWorker.terminate();
-                onDone(false);
-              }
-            }, opt_timeoutInMS || 1000);
-          }
-        }
-      };
-
-      myWorker.postMessage({ c: code, i: id });
-    }
     limitEval(message.content.substring(10), function(success, returnValue) {
       if (success) {
         var geval = eval;
