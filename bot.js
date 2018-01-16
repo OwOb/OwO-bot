50c5e2a0e0c53daa6bb7c9e39b9e41b2856fedc3
OwOb <35055831+OwOb@users.noreply.github.com>
2018-01-17 03:03:42 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ bot.on("message", function(message) {
     setTimeout(function(){
       //message.channel.sendMessage(eval("var a = 13; a*13"));
       //eval("while(1) {var a;};");
-      setTimeout(message.channel.sendMessage("www"), 5000);
+      setTimeout(message.channel.sendMessage("www");, 5000);
       for (var d = new Date().getTime(); 1; ) {
         if (d+3000 < new Date().getTime()) {
           message.channel.sendMessage("break");
