d3bdb56f7bb9893d7c69530386e43b6de0a5b373
OwOb <35055831+OwOb@users.noreply.github.com>
2018-01-18 23:23:04 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ bot.on("message", function(message) {
       //message.channel.sendMessage(eval("var a = 13; a*13"));
       //eval("while(1) {var a;};");
       try {
-        var testt = new Array(10000000000);
+        eval("function test() {test();}; setTimeout(test, 3000); while(1) {var a;};")
       }
       catch(e) {
         message.channel.sendMessage("QQ");
