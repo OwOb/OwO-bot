4279830b4b40cd002d655449326e291741a3fe71
OwOb <35055831+OwOb@users.noreply.github.com>
2019-02-19 20:50:37 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -461,7 +461,7 @@ bot.on("message", function(message) {
       })).on('parsed', function() {
         for (var y = 0; y < this.height; y++) {
           for (var x = 0; x < this.width; x++) {
-            var idx = (this.width*y+x)*3;
+            var idx = (this.width*y+x)<<2;
             this.data[idx] = 255-this.data[idx];
             this.data[idx+1] = 255-this.data[idx+1];
             this.data[idx+2] = 255-this.data[idx+2];
