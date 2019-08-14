e92b728a786e336273e756f814b3d5a0bba6cf3c
OwOb <35055831+OwOb@users.noreply.github.com>
2019-08-14 20:58:43 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -82,11 +82,11 @@ bot.on("ready", function() {
     }
   });
   
-  
+  /*
   bot.channels.get(process.env.bianyuanluzhai_voice_channel).join()
   .then(connection => console.log('Connected'))
   .catch(console.error);
-  
+  */
   
   console.log("Ready");
   /*
