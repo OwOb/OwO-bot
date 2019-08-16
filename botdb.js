170caff49f78f404e3f92e1b76966fe28c6ce316
OwOb <35055831+OwOb@users.noreply.github.com>
2019-08-16 08:45:00 +0800

    Update botdb.js

--- botdb.js
+++ botdb.js
@@ -16,11 +16,11 @@ module.exports = {
   
   newVar: function(varName, varValue, func) {
     botDB_Client.query("INSERT INTO Var_Table (var_name, value) VALUES ("+change39(varName)+", "+change39(varValue)+");", func);
-  }
+  },
   
   updateVar: function(varName, varValue, func) {
     botDB_Client.query("UPDATE Var_Table SET value = "+change39(varName)+" WHERE var_name = "+change39(varValue)+";", func);
-  }
+  },
   
   delVar: function(varName, func) {
     botDB_Client.query("DELETE FROM Var_Table SET WHERE var_name = "+change39(varName)+";", func);
