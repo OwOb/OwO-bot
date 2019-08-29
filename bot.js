28bc2b7596cb7f5fc44ef39e7b1168ccfc5ddc0e
OwOb <35055831+OwOb@users.noreply.github.com>
2019-08-30 00:05:50 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1138,7 +1138,7 @@ bot.on("message", message => {
                   var p_title = p_illust.title || "", p_image_url = pixiv_url(s_page-1 && s_page <= parseInt(p_illust.page_count) ? p_illust.manga_a[s_page-1].url_big : p_illust.url_big);
                   var p_user_name = p_user.user_name, p_user_icon = pixiv_url(p_user.profile_img.main), p_user_url = "https://www.pixiv.net/member.php?id="+p_user.user_id;
                   var p_des = p_illust.comment ? p_illust.comment.replace(/^\s+|\r+|\s$/g, "") : "", p_tags = p_illust.display_tags;
-                  var p_image_width = p_illust.illust_images[s_page].illust_image_width, p_image_height = p_illust.illust_images[s_page].illust_image_height, p_image_date = new Date(p_illust.upload_timestamp*1000);
+                  var p_image_width = p_illust.illust_images[s_page-1].illust_image_width, p_image_height = p_illust.illust_images[s_page-1].illust_image_height, p_image_date = new Date(p_illust.upload_timestamp*1000);
                   var p_view = parseInt(p_illust.rating_view), /*p_comment = p_illust.commentCount,*/ p_like = parseInt(p_illust.rating_count), p_bookmark = p_illust.bookmark_user_total;
                   var p_tags_string = "", p_original = false, p_r18 = false, p_r18g = false;
                   for (var index = 0; index < p_tags.length; index++) {
@@ -1151,7 +1151,7 @@ bot.on("message", message => {
                       p_r18g = true;
                     else {
                       var p_tag_tran = p_tag_index.translation, p_tag = "";
-                      if (p_tag_tran && (p_tag_tran.match(/[a-zA-Z]/g) == null || p_tag_tran.match(/[a-zA-Z]/g).length < p_tag_tran.length*0.75))
+                      if (p_tag_tran && (p_tag_tran.match(/[a-zA-Z]/g) == null || p_tag_tran.match(/[a-zA-Z\s]/g).length < p_tag_tran.length*0.8))
                         p_tag = p_tag_tran;
                       else
                         p_tag = p_tag_index.tag;
@@ -1162,7 +1162,7 @@ bot.on("message", message => {
                   if (p_r18) p_tags_string = "**R-18**　"+p_tags_string;
                   if (p_r18g) p_tags_string = "**R-18G**　"+p_tags_string;
                   if (p_original) p_tags_string = "**原創**　"+p_tags_string;
-                  console.log(p_json);
+                  //console.log(p_json);
                   richembed = richembed.setColor(38650).setThumbnail("https://i.imgur.com/UH7DQG8.png")
                                        .setTitle("__**\u200b"+dc_markdown(p_title)+"\u200b**__").setURL(s_url)
                                        .setAuthor(p_user_name, p_user_icon, p_user_url)
