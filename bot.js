7e6da5352ec707e6eaf5dc966e1f91cb5dc9a9e1
OwOb <35055831+OwOb@users.noreply.github.com>
2019-08-30 18:31:54 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -703,41 +703,43 @@ bot.on("message", message => {
   }
   
   else if (!isself && headlower == "!tex") {
-    if (args.length > 1) {
-      var texCommand = message.content.substring(headlower.length).replace(/(^\s*)|(\s*$)/g,"").replace(/((?!\n)\s)+/g," ");
-      var xmlhttp = new XMLHttpRequest();
-      xmlhttp.open("POST", 'http://latex2png.com/api/convert', true);
-      xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status < 300) {
-          var response = JSON.parse(xmlhttp.responseText);
-          if (!response["result-code"]) {
-            var imageURL = "http://latex2png.com"+response.url;
-            var imageName = "./"+imageURL.match(/[0-9a-f]+\.png/);
-            console.log(imageURL);
-            request(req_opt(imageURL)).pipe(new PNG()).on('parsed', function() {
-              var dst = new PNG({
-                width: this.width+20,
-                 height: this.height+20,
-                colorType: 2,
-                bgColor: { red: 54, green: 57, blue: 63}
+    channelTyping(message.channel, () => {
+      if (args.length > 1) {
+        var texCommand = message.content.substring(headlower.length).replace(/(^\s*)|(\s*$)/g,"").replace(/((?!\n)\s)+/g," ");
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("POST", 'http://latex2png.com/api/convert', true);
+        xmlhttp.onreadystatechange = function() {
+          if (xmlhttp.readyState == 4 && xmlhttp.status < 300) {
+            var response = JSON.parse(xmlhttp.responseText);
+            if (!response["result-code"]) {
+              var imageURL = "http://latex2png.com"+response.url;
+              var imageName = "./"+imageURL.match(/[0-9a-f]+\.png/);
+              console.log(imageURL);
+              request(req_opt(imageURL)).pipe(new PNG()).on('parsed', function() {
+                var dst = new PNG({
+                  width: this.width+20,
+                   height: this.height+20,
+                  colorType: 2,
+                  bgColor: { red: 54, green: 57, blue: 63}
+                });
+                this.bitblt(dst, 0, 0, this.width, this.height, 10, 10);
+                dst.pack().pipe(fs.createWriteStream(imageName)).on("close", function() {
+                  message.channel.send({files:[imageName]});
+                });
+              }).on('error', function(err) {
+                console.log(err);
+                message.channel.send("無法轉換成圖片！O3O\n請檢查TeX指令是否有誤！");
               });
-              this.bitblt(dst, 0, 0, this.width, this.height, 10, 10);
-              dst.pack().pipe(fs.createWriteStream(imageName)).on("close", function() {
-                message.channel.send({files:[imageName]});
-              });
-            }).on('error', function(err) {
-              console.log(err);
+            }
+            else
               message.channel.send("無法轉換成圖片！O3O\n請檢查TeX指令是否有誤！");
-            });
           }
-          else
-            message.channel.send("無法轉換成圖片！O3O\n請檢查TeX指令是否有誤！");
         }
+        xmlhttp.send(JSON.stringify({"auth": {"user": "guest", "password": "guest"}, "latex": texCommand, "resolution": 300,"color": "ffffff"}));
       }
-      xmlhttp.send(JSON.stringify({"auth": {"user": "guest", "password": "guest"}, "latex": texCommand, "resolution": 300,"color": "ffffff"}));
-    }
-    else
-      message.channel.send("沒給指令是要轉換什麼啦！(╯‵□ˊ)╯︵┴─┴\n指令格式: "+headlower+" [KaTeX指令]");
+      else
+        message.channel.send("沒給指令是要轉換什麼啦！(╯‵□ˊ)╯︵┴─┴\n指令格式: "+headlower+" [KaTeX指令]");
+    });
   }
   
   else if (!isself && (message.content.indexOf("什麼是") == 0 || headlower == ("!google"))) {
