7ada7f86618463c4fb31d49484e669091b9e81ba
OwOb <35055831+OwOb@users.noreply.github.com>
2019-08-31 00:56:22 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -430,6 +430,8 @@ bot.on("message", message => {
       var command_text = lowerargs[1], help_file = "";
       if (command_text == "查詢指令" || command_text == "help" || command_text == "man")
         help_file = "help";
+      else if (command_text == "程式執行" || command_text == "code")
+        help_file = "code";
       else if (command_text == "latex" || command_text == "tex")
         help_file = "latex";
       else if (command_text == "筆記" || command_text == "note")
