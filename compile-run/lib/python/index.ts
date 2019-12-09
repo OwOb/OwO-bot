7546c9f7dcafb90b686158de32f33e6e74c64ab3
OwOb <lw310659@gmail.com>
2019-12-09 23:41:38 +0800

    deleted

--- compile-run/lib/python/index.ts
+++
@@ -1,9 +0,0 @@
-import { runPythonFile } from './run-file';
-import { runPythonSourceCode } from './run-source';
-
-const python = {
-    runFile: runPythonFile,
-    runSource: runPythonSourceCode
-};
-
-export default python;
