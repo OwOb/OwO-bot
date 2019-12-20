fccd5084c43e76411767e9dbd9b4ab67dd8f3660
OwOb <lw310659@gmail.com>
2019-12-20 20:35:36 +0800

    modified

--- bot.js
+++ bot.js
@@ -125,7 +125,7 @@ bot.on("ready", function() {
   console.log("Ready");
   
   var nowTime = new Date();
-  var newYear = new Date("2019/12/20 08:35:00");
+  var newYear = new Date("2019/12/20 20:38:00");
   var timeZone = nowTime.getTimezoneOffset();
   var t = newYear.getTime()-28800000-nowTime.getTime()+timeZone*60000-100;
   if (t >= 0)
